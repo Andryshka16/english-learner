89ad2a5e00a704378a97c2ee405aa0b58cc41788
Andrey Lapchik <a.lapchik7@gmail.com>
2023-04-28 15:27:01 +0300

    fixed footer bug

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,9 +1,7 @@
 export default function Footer() {
 	return (
-		// <div className=''>
-		<div className='flex justify-center items-center mt-auto w-full h-12 px-20 text-white text-base bg-slate-500'>
+		<div className='flex justify-center items-center mt-auto w-full py-2 px-20 text-white text-base bg-slate-500'>
 			© 2023 WordScope All Rights Reserved
 		</div>
-		// </div>
 	)
 }
